73ee095c589cca14d7ff0f4774a9fcadc2fd8ee4
acse-68b0228d <2298500387@qq.com>
2022-10-10 15:17:08 +0100

    <my commit message>

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,11 @@
 import numpy as np
-
-
-__all__ = ['rand_array']
-
+from scipy.ndimage import gaussian_filter
+from scipy import misc
+__all__ = ['rand_array','smooth_image','my_mat_solve']
+def smooth_image(a, sigma=1):
+ return gaussian_filter(a, sigma=sigma)
 
 def rand_array(shape):
     return np.random.rand(*shape)
+def my_mat_solve(A, b):
+    return A.inv()*b
